0e70192364350a75c28bda940c76dbf6dcafcb1f
Muhammet Emin Gündoğar <407596@ogr.ktu.edu.tr>
2022-08-08 18:00:54 +0300

    splash added.

--- app/src/main/java/com/example/mesagingapp/LoginActivity.kt
+++ app/src/main/java/com/example/mesagingapp/LoginActivity.kt
@@ -2,10 +2,13 @@ package com.example.mesagingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_login)
     }
 
